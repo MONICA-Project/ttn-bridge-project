50f508c4769736df783f13747700f672e6e76666
BlubbFish <git@blubbfish.net>
2019-11-14 17:00:47 +0100

    Init

---
+++ update.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+echo "Pull Librarys/litjson"
+if [ -a Librarys/litjson/.git ] 
+then
+	cd Librarys/litjson
+	git pull
+	cd ..
+	cd ..
+else
+	git submodule update --init --  Librarys/litjson
+	cd Librarys/litjson
+	git checkout master
+	cd ..
+	cd ..
+fi
+
+echo "Pull Librarys/Mono.Posix"
+if [ -a Librarys/Mono.Posix/.git ] 
+then
+	cd Librarys/Mono.Posix
+	git pull
+	cd ..
+	cd ..
+else
+	git submodule update --init --  Librarys/Mono.Posix
+	cd Librarys/Mono.Posix
+	git checkout master
+	cd ..
+	cd ..
+fi
+
+echo "Pull Librarys/mqtt"
+if [ -a Librarys/mqtt/.git ] 
+then
+	cd Librarys/mqtt
+	git pull
+	cd ..
+	cd ..
+else
+	git submodule update --init --  Librarys/mqtt
+	cd Librarys/mqtt
+	git checkout master
+	cd ..
+	cd ..
+fi
+
+echo "Pull TTN-Bridge"
+if [ -a TTN-Bridge/.git ] 
+then
+	cd TTN-Bridge
+	git pull
+	cd ..
+else
+	git submodule update --init --  TTN-Bridge
+	cd TTN-Bridge
+	git checkout master
+	cd ..
+fi
+
+echo "Pull Utils/ConnectorDataMqtt"
+if [ -a Utils/ConnectorDataMqtt/.git ] 
+then
+	cd Utils/ConnectorDataMqtt
+	git pull
+	cd ..
+	cd ..
+else
+	git submodule update --init --  Utils/ConnectorDataMqtt
+	cd Utils/ConnectorDataMqtt
+	git checkout master
+	cd ..
+	cd ..
+fi
+
+echo "Pull Utils/Bot-Utils"
+if [ -a Utils/Bot-Utils/.git ] 
+then
+	cd Utils/Bot-Utils
+	git pull
+	cd ..
+	cd ..
+else
+	git submodule update --init --  Utils/Bot-Utils
+	cd Utils/Bot-Utils
+	git checkout master
+	cd ..
+	cd ..
+fi
+
+echo "Pull Utils/Utils-IoT"
+if [ -a Utils/Utils-IoT/.git ] 
+then
+	cd Utils/Utils-IoT
+	git pull
+	cd ..
+	cd ..
+else
+	git submodule update --init --  Utils/Utils-IoT
+	cd Utils/Utils-IoT
+	git checkout master
+	cd ..
+	cd ..
+fi
+
+if [ -z "$1" ]
+then
+	read -p "Please Press key"
+fi
